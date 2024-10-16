82591efe568703434db792222c79d145eaea481c
yadaram srikanth <yadaramsrikanth2000@gmail.com>
2024-10-16 13:59:08 +0530

    matrixlabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+const App=()=> <div className="main-container">
+<h1 className="heading">When Innovation <br/> Meets <span>Investment</span></h1>
+<p className="home-para">Empowering Trading Through Advanced Technology</p>
+<button className="open-button">Open dApp</button>
+<h4 className="feautures-heading">Our Feautures</h4>
+<div className="feauture-container">
+<div className="trade-optimizer-container">
+  <h6 className="trade-optimizer-heading">Trade Optimizer</h6>
+  <p className="trade-optimizer-para">Find the right moments to buy or sell, with personalized trade suggestions designed to help you make the most of every opportunity.</p>
+</div>
+<div className="market-insight-container">
+<h6 className="trade-optimizer-heading market">Market Insight</h6>
+<p className="market-insight-para">Stay ahead of the market. Get real-time updates on market trends, track top traders' movements, and spot signals from key influencers.</p>
+</div>
+<div className="risk-guard-container">
+<h6 className="trade-optimizer-heading">Risk Guard</h6>
+<p className="trade-optimizer-para">Get alerts on market swings and potential risks before they impact your portfolio. This agent helps you navigate volatility and stay prepared for anything.</p>
+</div>
+</div>
+<h2 className="about-ethai-heading">About EthAi</h2>
+<p className="about-ethai-para">At Eth Ai, we’re all about making crypto trading easier and more intuitive. We provide tools that help traders keep up with all new market trends, track top traders’ movements.</p>
+</div>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default App
